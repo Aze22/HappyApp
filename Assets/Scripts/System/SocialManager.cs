21ae2f99316a13209bc7901ff65d50a7d82c34ef
Charles Pearson <contact@haze-games.com>
2015-10-04 18:21:45 +0100

    Plugins
    Reporter
    Soomla Profile
    Login with facebook button not working yet

---
+++ Assets/Scripts/System/SocialManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using Soomla.Profile;
+
+[Prefab("SocialManager", true)]
+public class SocialManager : Singleton<SocialManager>
+{
+	public void Init() { SubscribeEvents(); }
+
+	public void SubscribeEvents()
+	{
+		ProfileEvents.OnLoginFinished += onLoginFinished;
+		ProfileEvents.OnLoginCancelled += onLoginCancelled;
+		ProfileEvents.OnLoginFailed += onLoginFailed;
+	}
+
+	public void onLoginFinished(UserProfile userProfileJson, bool autoLogin, string payload)
+	{
+		Debug.Log("Login to Facebook successful!");
+	}
+
+	public void onLoginCancelled(Provider provider, bool autoLogin, string payload)
+	{
+		Debug.Log("Login to Facebook has been cancelled!");
+	}
+
+	public void onLoginFailed(Provider provider, string message, bool autoLogin, string payload)
+	{
+		Debug.LogWarning("Login to Facebook has failed!");
+	}
+}
---
+++ Assets/Scripts/System/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Soomla.Profile;
+
+namespace HappyApp
+{
+    public class UIManager : MonoBehaviour
+    {
+        public View_Quote m_quoteView;
+        public Stack m_viewStack;
+
+        void Start()
+        {
+            HideAllViews(false);
+            m_quoteView.Show(true, 1f);
+        }
+
+        public void HideAllViews(bool _animated = true)
+        {
+            m_quoteView.Hide(_animated);
+        }
+
+		public void FacebookLoginPressed()
+		{
+			SoomlaProfile.Login(Provider.FACEBOOK);
+		}
+    }
+}
